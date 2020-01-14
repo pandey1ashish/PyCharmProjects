bc3d214e38180e80276e1d9833dbf5960913de9b
pandey1ashish <ashishpandey.list@gmail.com>
2020-01-14 15:43:32 -0500

    last commit from Linux on 15/Jan/2020

--- FirstWebProject/src/TestFlaskOne.py
+++ FirstWebProject/src/TestFlaskOne.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, request, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    render_template("ChldTemp.html") #, content="Testing")
+    render_template("temp.html", title="Title Value", content="Testing")
 
 
 if __name__ == "__main__":
